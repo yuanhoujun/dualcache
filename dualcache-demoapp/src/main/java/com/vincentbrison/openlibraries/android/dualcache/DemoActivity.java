2b863857b95fc17be6ccfebd7746c5afa87b9c5d
Scott Smith <626306805@qq.com>
2017-02-25 19:09:25 +0800

    使用AndCache修正缓存Demo

--- dualcache-demoapp/src/main/java/com/vincentbrison/openlibraries/android/dualcache/DemoActivity.java
+++ dualcache-demoapp/src/main/java/com/vincentbrison/openlibraries/android/dualcache/DemoActivity.java
@@ -10,6 +10,12 @@ import android.widget.Toast;
 
 import com.vb.openlibraries.android.dualcache.R;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.UUID;
 
 import butterknife.BindView;
@@ -38,8 +44,7 @@ public class DemoActivity extends Activity {
     private int mDiskCacheSize;
     private int mRamCacheSize;
     private String mCacheId;
-    private DualCache<String> mCache;
-
+    private AndCache mCache;
     private Handler mHandler;
 
     //CHECKSTYLE:OFF
@@ -66,13 +71,27 @@ public class DemoActivity extends Activity {
         mDiskCacheSize = getIntent().getIntExtra(EXTRA_DISK_CACHE_SIZE, 100);
         mRamCacheSize = getIntent().getIntExtra(EXTRA_RAM_CACHE_SIZE, 50);
 
-        CacheSerializer<String> jsonSerializer = new JsonSerializer<>(String.class);
+        mCache = new AndCacheBuilder(mCacheId , 1)
+                .enableLog()
+                .maxDiskSize(mDiskCacheSize)
+                .useReferenceInRam(mRamCacheSize, new SizeOf<Serializable>() {
+                    @Override
+                    public int sizeOf(Serializable object) {
+                        try {
+                            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                            ObjectOutputStream objectOutputStream = new ObjectOutputStream(bos);
+                            objectOutputStream.writeObject(object);
+                            objectOutputStream.flush();
+                            bos.flush();
 
-        mCache = new Builder<String>(mCacheId, 1)
-            .enableLog()
-            .useSerializerInRam(mRamCacheSize, jsonSerializer)
-            .useSerializerInDisk(mDiskCacheSize, true, jsonSerializer, getApplicationContext())
-            .build();
+                            return bos.toByteArray().length;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return 0;
+                    }
+                })
+                .build(this);
 
         mHandler = new Handler();
         mHandler.post(new Runnable() {
